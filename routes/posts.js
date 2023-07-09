a4bbea37e4b8916d57fb5da40e89bb607c3cecdb
Michal Abargel <mbrgl715@gmail.com>
2023-07-10 00:05:20 +0300

    Finish adding CRUD to posts, commetns, todos

--- routes/posts.js
+++ routes/posts.js
@@ -1,31 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../database/db');
+const db = require("../database/db");
 
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
 // GET all posts
 router.get("/", async (req, res) => {
+  console.log("handle GET req in posts");
+
   db.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to database:', err);
+      console.error("Error connecting to database:", err);
       //500 - Internal server error
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     // Prepare and execute the SQL query
-    const query = 'SELECT * FROM posts';
+    const query = "SELECT * FROM posts";
     connection.query(query, (err, results) => {
       connection.release();
 
       if (err) {
-        console.error('Error executing query:', err);
+        console.error("Error executing query:", err);
         //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        return res.status(500).send("An error occurred");
       } else if (results.length === 0) {
-          //404 - Data not found
-        return res.status(404).send('Data not found');
+        //404 - Data not found
+        return res.status(404).send("Data not found");
       } else {
         res.json(results);
       }
@@ -35,97 +37,172 @@ router.get("/", async (req, res) => {
 
 // GET all posts of user
 router.get("/:userid", async (req, res) => {
+  console.log("handle GET req in posts with userId");
 
-    // Retrieve user ID from request parameters
-    const userId = req.params.userid;
-  
-    // Connect to the database
-    db.getConnection((err, connection) => {
+  // Retrieve user ID from request parameters
+  const userId = req.params.userid;
+
+  // Connect to the database
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+      return res.status(500).send("An error occurred");
+    }
+
+    // Prepare and execute the SQL query
+    const query = "SELECT * FROM posts WHERE userId = ?";
+    connection.query(query, [userId], (err, results) => {
+      connection.release();
+
       if (err) {
-        console.error('Error connecting to database:', err);
+        console.error("Error executing query:", err);
         //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        return res.status(500).send("An error occurred");
+      } else if (results.length === 0) {
+        //404 - User's posts not found
+        return res.status(404).send("User's posts not found");
+      } else {
+        //200- ok
+        res.status(200).json(results);
       }
-  
-      // Prepare and execute the SQL query
-      const query = 'SELECT * FROM posts WHERE userId = ?';
-      connection.query(query, [userId], (err, results) => {
-        connection.release();
-  
-        if (err) {
-          console.error('Error executing query:', err);
-          //500 - Internal server error
-          return res.status(500).send('An error occurred');
-        } else if (results.length === 0) {
-            //404 - User's posts not found
-          return res.status(404).send("User's posts not found");
-        } else {
-          //200- ok
-          res.status(200).json(results);
-        }
-      });
     });
+  });
 });
 
 // GET post by id and userid
 router.get("/:userid/:postid", async (req, res) => {
-  const {userId, postid} = req.params;
- db.getConnection((err, connection) => {
+  console.log("handle GET req in posts with userId and Id");
+
+  const { userId, postid } = req.params;
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+      return res.status(500).send("An error occurred");
+    }
+
+    // Prepare and execute the SQL query
+    const query = "SELECT * FROM posts WHERE id = ?";
+    connection.query(query, [postid], (err, results) => {
+      connection.release();
+
       if (err) {
-        console.error('Error connecting to database:', err);
+        console.error("Error executing query:", err);
         //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        return res.status(500).send("An error occurred");
+      } else if (results.length === 0) {
+        //404 - Post not found
+        return res.status(404).send("Post not found");
+      } else {
+        //200- Success action
+        res.status(200).json(results[0]);
       }
-  
-      // Prepare and execute the SQL query
-      const query = 'SELECT * FROM posts WHERE id = ?';
-      connection.query(query, [postid], (err, results) => {
-        connection.release();
-  
-        if (err) {
-          console.error('Error executing query:', err);
-          //500 - Internal server error
-          return res.status(500).send('An error occurred');
-        } else if (results.length === 0) {
-            //404 - Post not found
-          return res.status(404).send('Post not found');
-        } else {
-          //200- Success action
-          res.status(200).json(results[0]);
-        }
-      });
     });
+  });
 });
 
 // POST add post
 router.post("/", (req, res) => {
+  console.log("handle POST req in posts");
+
   console.log("handle post req");
   console.log(req.body);
-  const {userId, title, body} = req.body;
-  
-    // Connect to the database
-    db.getConnection((err, connection) => {
+  const { userId, title, body } = req.body;
+
+  // Connect to the database
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+      return res.status(500).send("An error occurred");
+    }
+
+    // Prepare and execute the SQL query
+    const query = "INSERT INTO posts (userId, title, body) VALUES (?, ?)";
+    connection.query(query, [userId, title, body], (err, results) => {
+      connection.release();
+
       if (err) {
-        console.error('Error connecting to database:', err);
+        console.error("Error executing query:", err);
         //500 - Internal server error
-        return res.status(500).send('An error occurred');
-      }
-  
-      // Prepare and execute the SQL query
-      const query = 'INSERT INTO posts (userId, title, body) VALUES (?, ?)';
-      connection.query(query, [userId, title, body], (err, results) => {
-        connection.release();
-  
-        if (err) {
-          console.error('Error executing query:', err);
-        //500 - Internal server error
-          return res.status(500).send('An error occurred');
-        } else {
+        return res.status(500).send("An error occurred");
+      } else {
         //201 - Poat created successfully
-          res.status(201).json({ message: 'Post created successfully' });
-        }
-      });
+        res.status(201).json({ message: "Post created successfully" });
+      }
     });
+  });
+});
+
+//PUT = update post
+router.put("/", (req, res) => {
+  console.log("handle PUT req in posts");
+
+  // Retrieve user ID and updated data from request parameters and body
+  const postId = req.params.id;
+  const { title, body } = req.body;
+
+  // Connect to the database
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+      return res.status(500).send("An error occurred");
+    }
+
+    // Prepare and execute the SQL query
+    const query = "UPDATE posts SET title = ?, body = ? WHERE id = ?";
+    connection.query(query, [title, body, postId], (err, results) => {
+      connection.release();
+
+      if (err) {
+        console.error("Error executing query:", err);
+        //500 - Internal server error
+        return res.status(500).send("An error occurred");
+      } else if (results.affectedRows === 0) {
+        //404 - User not found
+        return res.status(404).send("User not found");
+      } else {
+        res.json({ message: "User updated successfully" });
+      }
+    });
+  });
+});
+
+//Delete post
+router.delete("/", (req, res) => {
+  console.log("handle DELTET req in posts");
+
+  // Retrieve user ID from request parameters
+  const postId = req.params.id;
+
+  // Connect to the database
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+
+      return res.status(500).send("An error occurred");
+    }
+
+    // Prepare and execute the SQL query
+    const query = "DELETE FROM posts WHERE id = ?";
+    connection.query(query, [postId], (err, results) => {
+      connection.release();
+
+      if (err) {
+        console.error("Error executing query:", err);
+        //500 - Internal server error
+        return res.status(500).send("An error occurred");
+      } else if (results.affectedRows === 0) {
+        //404 - User not found
+        return res.status(404).send("User not found");
+      } else {
+        res.json({ message: "User deleted successfully" });
+      }
+    });
+  });
 });
 
 module.exports = router;
--- routes/todos.js
+++ routes/todos.js
@@ -1,40 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../database/db');
+const db = require("../database/db");
 
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 
-
-
-
-
 //GET TODOS
 router.get("/:userid", async (req, res) => {
+  console.log("handle GET req in todos");
+
   // Retrieve user ID from request parameters
   const userId = req.params.userid;
 
   // Connect to the database
   db.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to database:', err);
+      console.error("Error connecting to database:", err);
       //500 - Internal server error
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     // Prepare and execute the SQL query
-    const query = 'SELECT * FROM todos WHERE userId = ?';
+    const query = "SELECT * FROM todos WHERE userId = ?";
     connection.query(query, [userId], (err, results) => {
       connection.release();
 
       if (err) {
-        console.error('Error executing query:', err);
+        console.error("Error executing query:", err);
         //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        return res.status(500).send("An error occurred");
       } else if (results.length === 0) {
-          //404 - Todos not found
-          console.log(results);
-        return res.status(404).send('Todos not found');
+        //404 - Todos not found
+        console.log(results);
+        return res.status(404).send("Todos not found");
       } else {
         res.status(200).json(results);
       }
@@ -43,96 +41,102 @@ router.get("/:userid", async (req, res) => {
 });
 
 //POST = create todo
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  console.log("handle POST req in todos");
+
   // Extract user data from the request body
-  const {userId, title, completed} = req.body;
+  const { userId, title, completed } = req.body;
 
   // Connect to the database
   db.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to database:', err);
+      console.error("Error connecting to database:", err);
       //500 - Internal server error
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     // Prepare and execute the SQL query
-    const query = 'INSERT INTO users (userId, title, completed) VALUES (?, ?)';
+    const query = "INSERT INTO todos (userId, title, completed) VALUES (?, ?)";
     connection.query(query, [userId, title, completed], (err, results) => {
       connection.release();
 
       if (err) {
-        console.error('Error executing query:', err);
-      //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        console.error("Error executing query:", err);
+        //500 - Internal server error
+        return res.status(500).send("An error occurred");
       } else {
-      //201 - Todo created successfully
-        res.status(201).json({ message: 'Todo created successfully' });
+        //201 - Todo created successfully
+        res.status(201).json({ message: "Todo created successfully" });
       }
     });
   });
 });
 
 //PUT = update
-router.put('/', (req, res) => {
+router.put("/", (req, res) => {
+  console.log("handle PUT req in todos");
+
   // Retrieve user ID and updated data from request parameters and body
-  const userId = req.params.id;
-  const { name, email } = req.body;
+  const todoId = req.params.id;
+  const { title, completed } = req.body;
 
   // Connect to the database
   db.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to database:', err);
-              //500 - Internal server error
-      return res.status(500).send('An error occurred');
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
+      return res.status(500).send("An error occurred");
     }
 
     // Prepare and execute the SQL query
-    const query = 'UPDATE users SET name = ?, email = ? WHERE id = ?';
-    connection.query(query, [name, email, userId], (err, results) => {
+    const query = "UPDATE todos SET title = ?, completed = ? WHERE id = ?";
+    connection.query(query, [title, completed, todoId], (err, results) => {
       connection.release();
 
       if (err) {
-        console.error('Error executing query:', err);
-                //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        console.error("Error executing query:", err);
+        //500 - Internal server error
+        return res.status(500).send("An error occurred");
       } else if (results.affectedRows === 0) {
-          //404 - User not found
-        return res.status(404).send('User not found');
+        //404 - User not found
+        return res.status(404).send("User not found");
       } else {
-        res.json({ message: 'User updated successfully' });
+        res.json({ message: "User updated successfully" });
       }
     });
   });
 });
 
-//Delete
-router.delete('/', (req, res) => {
+//Delete todo
+router.delete("/", (req, res) => {
+  console.log("handle DELETE req in todos");
+
   // Retrieve user ID from request parameters
-  const userId = req.params.id;
+  const todoId = req.params.id;
 
   // Connect to the database
   db.getConnection((err, connection) => {
     if (err) {
-      console.error('Error connecting to database:', err);
-          //500 - Internal server error
+      console.error("Error connecting to database:", err);
+      //500 - Internal server error
 
-      return res.status(500).send('An error occurred');
+      return res.status(500).send("An error occurred");
     }
 
     // Prepare and execute the SQL query
-    const query = 'DELETE FROM users WHERE id = ?';
-    connection.query(query, [userId], (err, results) => {
+    const query = "DELETE FROM todos WHERE id = ?";
+    connection.query(query, [todoId], (err, results) => {
       connection.release();
 
       if (err) {
-        console.error('Error executing query:', err);
-                //500 - Internal server error
-        return res.status(500).send('An error occurred');
+        console.error("Error executing query:", err);
+        //500 - Internal server error
+        return res.status(500).send("An error occurred");
       } else if (results.affectedRows === 0) {
-          //404 - User not found
-        return res.status(404).send('User not found');
+        //404 - User not found
+        return res.status(404).send("User not found");
       } else {
-        res.json({ message: 'User deleted successfully' });
+        res.json({ message: "User deleted successfully" });
       }
     });
   });
