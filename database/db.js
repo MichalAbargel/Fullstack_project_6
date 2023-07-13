b0aeee24a896843a3154f58bc664a914fdfab0ef
Michal Abargel <mbrgl715@gmail.com>
2023-07-13 16:24:00 +0300

    handle replace db

--- database/db.js
+++ database/db.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql2");
-const db = null;
-try {
+const user = 1; // inorder to replace db need to chagne the number
+let db = null;
+if (user === 1) {
+  db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "Michal6354!",
+    port: 3306,
+    database: "FullStackProject6",
+  });
+} else {
   db = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "Mysql1864",
-    port: 3306,
-    database: "FullStackProject6",
-  });
-} catch (E) {
-  db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Michal635!",
     port: 3306,
     database: "FullStackProject6",
   });
